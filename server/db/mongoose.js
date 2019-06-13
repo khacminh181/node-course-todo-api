872a300c88532df1b19bdb0b1df6fe15e0abe4bd
nguyenkhacminh181@gmail.com <nguyenkhacminh181@gmail.com>
2019-06-13 09:52:53 +0700

    asd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://admin1:minh1234@ds013486.mlab.com:13486/node-todos-app' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect('mongodb://admin1:minh1234@ds013486.mlab.com:13486/node-todos-app' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
-if(process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
+// if(process.env.MONGODB_URI) {
+//     mongoose.connect(process.env.MONGODB_URI);
    
-   }else {
+//    }else {
    
-    mongoose.connect(db, function(err){ //db = 'mongodb://localhost/yourdb'
-     if(err){
-      console.log(err);
-     }else {
-      console.log('mongoose connection is successful on: ' + db);
-     }
-    });
-   }
+//     mongoose.connect(db, function(err){ //db = 'mongodb://localhost/yourdb'
+//      if(err){
+//       console.log(err);
+//      }else {
+//       console.log('mongoose connection is successful on: ' + db);
+//      }
+//     });
+//    }
 module.exports = {mongoose};

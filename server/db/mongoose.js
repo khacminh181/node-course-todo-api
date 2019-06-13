c0ee9eba8377555aeb51cee4bcf752d188496df6
nguyenkhacminh181@gmail.com <nguyenkhacminh181@gmail.com>
2019-06-13 09:53:52 +0700

    asd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin1:minh1234@ds013486.mlab.com:13486/node-todos-app' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 // if(process.env.MONGODB_URI) {
 //     mongoose.connect(process.env.MONGODB_URI);
